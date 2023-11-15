01008dc1dfa0be532f3ab576448c94eb82abfb7e
Fatima Kammona <yourgitemail@example.com>
2023-11-15 16:01:00 -0600

    Added basic connection.

--- client.py
+++ client.py
@@ -1,6 +1,30 @@
 import sys
 import socket
 
-serverName = sys.argv[1] 
-serverPort = int(sys.argv[2]) 
-studentID = sys.argv[3]
+def main():
+    # Ensure that the server name and port number are provided
+    if len(sys.argv) < 4:
+        print("Usage: python client.py <ServerName> <ServerPort> <Message>")
+        return
+
+    serverName = sys.argv[1]        # Server IP address
+    serverPort = int(sys.argv[2])   # Port number of the server to connect to
+    message = sys.argv[3]           # Message to send to the server
+
+    # Create a TCP/IP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+        # Connect to the server
+        clientSocket.connect((serverName, serverPort))
+        
+        # Send a message to the server
+        clientSocket.send(message.encode())
+
+        # Receive the server response
+        response = clientSocket.recv(1024).decode()
+        print(f"From Server: {response}")
+
+        # Close the connection (done implicitly by the with statement)
+
+if __name__ == "__main__":
+    main()
+
--- server.py
+++ server.py
@@ -1,6 +1,38 @@
-import sys
 import socket
 
-serverName = sys.argv[1] 
-serverPort = int(sys.argv[2]) 
-studentID = sys.argv[3]
+def main():
+    # Set the server's port number
+    serverPort = 12000  
+
+    # Create a TCP/IP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+        # Bind the socket to the port
+        serverSocket.bind(('', serverPort))
+        
+        # Listen for incoming connections
+        serverSocket.listen(1)
+        print(f"The server is ready to receive on port {serverPort}")
+        
+        # Keep the server running forever
+        while True:
+            # Wait for a connection
+            connectionSocket, addr = serverSocket.accept()
+            
+            # Receive data from the client
+            message = connectionSocket.recv(1024).decode()
+            print(f"Received from {addr}: {message}")
+
+            if message == "quit":
+                print("Server received termination signal. Closing connection.")
+                connectionSocket.close()
+                break
+            else:
+                # Send a response back to the client
+                response = f"Message received: {message}"
+                connectionSocket.send(response.encode())
+            
+            # Close the connection
+            connectionSocket.close()
+
+if __name__ == "__main__":
+    main()
